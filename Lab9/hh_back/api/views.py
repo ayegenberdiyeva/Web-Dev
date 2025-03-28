8c76299f42a1b2a8e73b213d46069717ad4287e2
Amina <aminayegnb@gmail.com>
2025-03-28 16:26:15 +0500

    Lab9

---
+++ Lab9/hh_back/api/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Company, Vacancy
+from .serialazers import CompanySerializer, VacancySerializer
+
+@api_view(['GET'])
+def company_list(request):
+    companies = Company.objects.all()
+    serializer = CompanySerializer(companies, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def company_detail(request, id):
+    try:
+        company = Company.objects.get(pk=id)
+        serializer = CompanySerializer(company)
+        return Response(serializer.data)
+    except Company.DoesNotExist:
+        return Response({"error": "Company not found"}, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['GET'])
+def company_vacancy(request, id):
+    try:
+        vacancies = Vacancy.objects.filter(company_id=id)
+        serializer = VacancySerializer(vacancies, many=True)
+        return Response(serializer.data)
+    except Company.DoesNotExist:
+        return Response({"error": "Company not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+@api_view(['GET'])
+def vacancy_list(request):
+    vacancies = Vacancy.objects.all()
+    serializer = VacancySerializer(vacancies, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def vacancy_detail(request, id):
+    try:
+        vacancy = Vacancy.objects.get(pk=id)
+        serializer = VacancySerializer(vacancy)
+        return Response(serializer.data)
+    except Vacancy.DoesNotExist:
+        return Response({"error": "Vacancy not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+@api_view(['GET'])
+def top_ten_vacancies(request):
+    vacancies = Vacancy.objects.order_by('-salary')[:10]
+    serializer = VacancySerializer(vacancies, many=True)
+    return Response(serializer.data)
